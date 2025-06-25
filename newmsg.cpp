b6ffa51e73e17384dec66373bb44466c5275138a
MEMAD <spmeeakabg@gmail.com>
2025-06-25 22:55:58 +0300

    Add files via upload

---
+++ newmsg.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+cout <<"Hello this is M.Emad " << endl;
+cout <<"Email: spmeeakabg@gmail.com " << endl;
+cout <<"I'll respond as soon as possible" << endl;
+
+
+return 0;
+}
